a33a892c8bc720235874afa5ab8aa9d14a04b43e
Racher <81470900+Racher612@users.noreply.github.com>
2023-03-06 23:33:27 +0300

    06 03 23

--- 2 семестр/Физика/prog_helpers/test 1.py
+++ 2 семестр/Физика/prog_helpers/test 1.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from random import randint
 
 class lab:
     def prt(self):
         print(self.build_table())
-        print('xsr = ', self.sr)
-        print('sum = ', sum(self.data))
+        print('xsr = ', round(self.sr, 2))
+        print('sum = ', round(sum(self.data), 2))
 
     def build_table(self):
         return pd.DataFrame(data = {'xi' : self.data,
@@ -18,11 +19,26 @@ class lab:
         with open("data.csv", 'w') as f:
             df.to_csv(f, lineterminator = '\n', sep = ';', decimal = ',')
 
+    def rnd(self, pam, dest):
+        pam = int(pam*100)
+        if dest == -1:
+            for i in range(pam, pam - 5, -1):
+                if i % 5 == 0:
+                    return i
+        elif dest == 1:
+            for i in range(pam, pam + 5):
+                if i % 5 == 0:
+                    return i
+        else:
+            raise ValueError
 
     def histogram(self):
         fig, ax = plt.subplots(1, 1)
+        b = list(map(lambda x: x / 100, range(lab.rnd(self, min(self.data), -1), lab.rnd(self, max(self.data), 1) + 1, 5)))
         ax.hist(self.table['xi'], color='blue', edgecolor='black',
-                 bins = len(set(self.data)), bottom = min(data))
+                 bins = b, bottom = min(b))
+
+        ax.set_xlim([b[0], b[-1]])
 
         # Set title
         ax.set_title("Диаграмма распределения результатов измерения")
@@ -83,7 +99,7 @@ data = list(map(float, """1.96
             1.95
             1.89
             2.00
-            1.56
+            1.76
             2.06
             1.91
             1.88
@@ -99,6 +115,8 @@ data3 = [1.38, 1.23, 1.35, 1.40]
 data4 = list(map(lambda x: x - 0.5, [1.90, 1.74, 1.68, 1.91, 1.81, 2.06, 1.70, 1.70, 1.75, 1.82, 1.83, 1.80]))
 
 obj = lab(data1)
+obj.build_table()
+obj.prt()
 obj.histogram()
-#print(obj.build_table())
+
 #obj.excel_out()
